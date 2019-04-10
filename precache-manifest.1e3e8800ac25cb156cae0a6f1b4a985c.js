34e93763c44b69160373237606547083d39d0d4c
Donghui Ma <donghui.m.@gmail.com>
2019-04-11 01:57:33 +0200

    Updates

---
+++ precache-manifest.1e3e8800ac25cb156cae0a6f1b4a985c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56a271147b4cfa32c3c37c6fa3f08225",
+    "url": "/foodStock/static/media/mobile.56a27114.jpg"
+  },
+  {
+    "revision": "05ca33868a194f21a812",
+    "url": "/foodStock/static/js/runtime~main.cc4a190f.js"
+  },
+  {
+    "revision": "38822bcda811482503cb",
+    "url": "/foodStock/static/js/main.1547b1ed.chunk.js"
+  },
+  {
+    "revision": "a76cba15e7ef5affafc4",
+    "url": "/foodStock/static/js/2.5d848ac4.chunk.js"
+  },
+  {
+    "revision": "38822bcda811482503cb",
+    "url": "/foodStock/static/css/main.ea1a285e.chunk.css"
+  },
+  {
+    "revision": "4bc52bb9855bbb320c799ebb32b224c5",
+    "url": "/foodStock/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.647649281ce428761082832f9f68ea2c.js"
+  "/foodStock/precache-manifest.1e3e8800ac25cb156cae0a6f1b4a985c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/foodStock/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
