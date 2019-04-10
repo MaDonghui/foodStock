7a594f72f0e72cd74ab2fc048b2b9462fff64478
Donghui Ma <donghui.m.@gmail.com>
2019-04-11 01:40:46 +0200

    Updates

---
+++ precache-manifest.e664d22271344253549c4d156d86ad35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56a271147b4cfa32c3c37c6fa3f08225",
+    "url": "madonghui.github.io/static/media/mobile.56a27114.jpg"
+  },
+  {
+    "revision": "cfe5069d140932bcef03",
+    "url": "madonghui.github.io/static/js/runtime~main.baf13b74.js"
+  },
+  {
+    "revision": "76f7cdce90e6ce03528a",
+    "url": "madonghui.github.io/static/js/main.43fe39c7.chunk.js"
+  },
+  {
+    "revision": "a76cba15e7ef5affafc4",
+    "url": "madonghui.github.io/static/js/2.5d848ac4.chunk.js"
+  },
+  {
+    "revision": "76f7cdce90e6ce03528a",
+    "url": "madonghui.github.io/static/css/main.5a056cfa.chunk.css"
+  },
+  {
+    "revision": "2f68773ad3ae3cd95173263c58403697",
+    "url": "madonghui.github.io/index.html"
+  }
+];
