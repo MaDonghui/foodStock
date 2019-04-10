18dfb5e21008d90f0083c90a133677e4b2942708
Donghui Ma <donghui.m.@gmail.com>
2019-04-11 01:24:54 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import BusinessList from "./components/BusinessList/BusinessList";
+import SearchBar from "./components/SearchBar/SearchBar";
+
+import Yelp from "./util/Yelp.js";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({ businesses: businesses });
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Food<br></br>Stock</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
+    );
+  }
+}
